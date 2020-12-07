ee7fbea625a5846867e14b500ce397b5a96a1131
Lizator <frederikkoefoeds@gmail.com>
2020-12-07 16:35:05 +0100

    star 2 completed

--- day7.py
+++ day7.py
@@ -1,9 +1,44 @@
 
 
-isContainedBy = {}
+contains = {}
 found = []
-res = [-1]
 
+
+def countWithin(color):
+    res = 1
+    if contains[color][0][1] == "NONE":
+        return 1
+
+    for ls in contains[color]:
+        res += ls[0] * countWithin(ls[1])
+
+    return res
+
+
+for i in range(594):
+    line = input()
+    line = line.split(" bags contain ")
+    currColor = line[0]
+    if line[1] == "no other bags.":
+        contains[currColor] = [[0, "NONE"]]
+        pass
+    else:
+        insideList = line[1].split(", ")
+        for bags in insideList:
+            insideColor = bags.split(" ")[1] + " " + bags.split(" ")[2]
+            count = int(bags.split(" ")[0])
+            if currColor in contains.keys():
+                contains[currColor].append([count, insideColor])
+            else:
+                contains[currColor] = [[count, insideColor]]
+
+print(countWithin("shiny gold") - 1)
+
+
+""" # first star
+
+isContainedBy = {}
+res = [-1] # -1 for shiny gold bag not counting
 
 def dive(color):
     if not color in found:
@@ -27,5 +62,4 @@ for i in range(594):
             isContainedBy[insideColor] = [currColor]
 
 dive("shiny gold")
-
-print(res[0])
+"""
