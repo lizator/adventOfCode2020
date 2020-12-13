21481764ea7fef07a7f8e43f1070b220fa9984cd
Lizator <frederikkoefoeds@gmail.com>
2020-12-13 15:56:06 +0100

    day12 second star

--- day12.py
+++ day12.py
@@ -1,32 +1,41 @@
 
+def rotate(point):
+    x = point[0]
+    y = point[1]
+    newY = -1 * x
+    newX = y
+    return [newX, newY]
 
-directions = ["E", "S", "W", "N"]
-currDir = 0
+
+        #   x   y
+waypoint = [10, 1]
+
+
 x = 0
 y = 0
 for i in range(755):
     line = input()
 
     if line[0] == "F":
-        line = directions[currDir] + line[1:]
-
-    if i == 14:
-        print("test")
+        x += int(line[1:]) * waypoint[0]
+        y += int(line[1:]) * waypoint[1]
 
     if line[0] == "E":
-        x += int(line[1:])
+        waypoint[0] += int(line[1:])
     elif line[0] == "W":
-        x -= int(line[1:])
+        waypoint[0] -= int(line[1:])
     elif line[0] == "N":
-        y += int(line[1:])
+        waypoint[1] += int(line[1:])
     elif line[0] == "S":
-        y -= int(line[1:])
+        waypoint[1] -= int(line[1:])
     elif line[0] == "R":
         rot = int(int(line[1:]) / 90)
-        currDir = (currDir + rot) % 4
+        for j in range(rot):
+            waypoint = rotate(waypoint)
     elif line[0] == "L":
         rot = int(int(line[1:]) / 90)
-        currDir = int(currDir + 4 - rot) % 4
+        for j in range(4 - rot):
+            waypoint = rotate(waypoint)
 
 
 print(abs(x) + abs(y))
