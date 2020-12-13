25363c9bde5e8432de44b41f2c9d7b854bd9bc75
Lizator <frederikkoefoeds@gmail.com>
2020-12-13 14:55:18 +0100

    day11 start2

--- day11.py
+++ day11.py
@@ -20,14 +20,33 @@ def findSeating(currGrid, y, x):
     if sideCounter != -1:
         for j in range(5):
             side = sides[(j + sideCounter) % 8]
-            if currGrid[y + side[0]][x + side[1]] != -1:
-                res += currGrid[y + side[0]][x + side[1]]
+            curry = y
+            currx = x
+            freeView = False
+            while currGrid[curry + side[0]][currx + side[1]] == -1:
+                curry += side[0]
+                currx += side[1]
+                if not (0 <= curry + side[0] <= len(currGrid) - 1 and 0 <= currx + side[1] <= len(currGrid[0]) - 1):
+                    freeView = True
+                    break
+
+            if not freeView:
+                res += currGrid[curry + side[0]][currx + side[1]]
         return res
 
     for j in range(8):
         side = sides[j]
-        if currGrid[y + side[0]][x + side[1]] != -1:
-            res += currGrid[y + side[0]][x + side[1]]
+        curry = y
+        currx = x
+        freeView = False
+        while currGrid[curry + side[0]][currx + side[1]] == -1:
+            curry += side[0]
+            currx += side[1]
+            if not (0 < curry < len(currGrid) - 1 and 0 < currx < len(currGrid[0]) - 1):
+                freeView = True
+                break
+        if not freeView:
+            res += currGrid[curry + side[0]][currx + side[1]]
     return res
 
 
@@ -44,6 +63,7 @@ for i in range(97):
 
 oldGrid = []
 while True:
+
     if oldGrid != []:
         changed = False
         for y in range(len(grid)):
@@ -61,7 +81,7 @@ while True:
         for x in range(len(grid[0])):
             if oldGrid[y][x] != -1:
                 found = findSeating(oldGrid, y, x)
-                if found >= 4:
+                if found >= 5:
                     grid[y][x] = 0
                 elif found == 0:
                     grid[y][x] = 1
